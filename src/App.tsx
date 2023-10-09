507bc5bc57f40377384404d5063ca8e8d56a7634
BennyNoyman <bn0504127633@gmail.com>
2023-10-09 14:15:44 +0300

    badge

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import { Example } from "./components/Example/Example";
 import Basic from "./components/basic/Basic.tsx";
+import Cart from "./components/basic/Cart.tsx";
 
 function App() {
   return (
     <>
       <Example />{" "}
         <Basic/>
+        <Cart/>
     </>
   );
 }
